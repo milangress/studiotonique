f7f4c484db215e670ed80dec67d6e5909ca5043f
stinlemp <you@example.com>
2015-06-27 10:05:18 +0200

    added blog to menu

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -1,15 +1,1 @@
 <?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-email: alex@studiotonique.com
-password: >
-  $2a$10$NlMV859PS.pfkunQZMpaaeKNxfo4Wky/NkaQ3v44HaB6Va8Zmq.nS
-language: de
-role: admin
-firstname: Alexander
-lastname: Horn
-passwordconfirmation:
-history:
-  - projects/a-u-r-a
-  - info
-  - home
---
+++ site/templates/review.php
@@ -0,0 +1,5 @@
+<?php snippet('header') ?>
+
+<iframe id="blog" src="http://www.studiotonique.tumblr.com/" ></iframe>
+
+<?php snippet('footer') ?>
